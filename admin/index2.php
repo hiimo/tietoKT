7232e79be62ebda4d20cfd606339dcb248a1ea01
hiimo <115462482+hiimo@users.noreply.github.com>
2025-06-02 15:59:53 +0300

    Update index2.php

--- admin/index2.php
+++ admin/index2.php
@@ -7,7 +7,6 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
     exit;
 }
 ?>
-<!-- siin enamus on pm sama mis peamises index lehes niiet maj hakka ko0mmenteerima -->
 <!DOCTYPE html>
 <html lang="et">
 <head>
